107d62052e80de8383866cd4b27cee18b2d64b13
Eamon Hyland <ehyland90@gmail.com>
2021-10-02 11:23:46 +1000

    Set SCRATH_PATH to tmp dir

--- tests/e2e/test-utils.ts
+++ tests/e2e/test-utils.ts
@@ -5,7 +5,11 @@ export type ResolveType<T> = T extends (...args: any[]) => Promise<infer R>
   ? R
   : never;
 
-export const SCRATH_PATH = path.resolve('tests/e2e/scratch');
+export const SCRATH_PATH = process.env.SCRATH_PATH!;
+
+if (!SCRATH_PATH) {
+  throw new Error('Missing environment variable SCRATH_PATH');
+}
 
 export async function clearScratchSpace() {
   await fs.emptyDir(SCRATH_PATH);
