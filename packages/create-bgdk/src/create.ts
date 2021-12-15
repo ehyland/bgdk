8cd4cc6cb0cbe3d48208cfd2cbc94dd391802449
Eamon Hyland <ehyland90@gmail.com>
2021-12-15 22:32:57 +1100

    Create project with new package

--- packages/create-bgdk/src/create.ts
+++ packages/create-bgdk/src/create.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs-extra';
 import { log } from './log';
+import chalk from 'chalk';
 
 interface Options {
   appPath: string;
@@ -13,11 +14,37 @@ export async function main(options: Options) {
 
   log(`Creating app in ${outputPathRelative}`);
 
+  if (await fs.pathExists(outputPath)) {
+    const { isDirectory } = await fs.stat(outputPath);
+
+    if (!isDirectory) {
+      log(`File exists at output path ${outputPathRelative}`);
+      process.exit(1);
+    }
+
+    const files = await fs.readdir(outputPath);
+
+    if (files.length !== 0) {
+      log(`${outputPathRelative} is not empty`);
+      process.exit(1);
+    }
+  }
+
   await fs.copy(templatePath, outputPath, { overwrite: true });
 
-  log(`App created! Next steps
+  // Fix .gitignore. See [issue](https://github.com/npm/npm/issues/1862).
+  try {
+    await fs.move(
+      path.resolve(outputPath, '.npmignore'),
+      path.resolve(outputPath, '.gitignore'),
+      { overwrite: true },
+    );
+  } catch (error) {
+    // assuming this is local dev
+  }
 
-  $ cd ${outputPathRelative}
-  $ yarn install
+  log(`App created! Next steps
+  $ ${chalk.whiteBright(`cd ${outputPathRelative}`)}
+  $ ${chalk.whiteBright(`yarn install`)}
   `);
 }
--- tests/e2e/commands/create.test.ts
+++ tests/e2e/commands/create.test.ts
@@ -6,29 +6,37 @@ import { ResolveType, clearScratchSpace, SCRATCH_PATH } from '../test-utils';
 
 jest.setTimeout(ms('5 minutes'));
 
+async function cmd(
+  cmdString: string,
+  options?: Omit<execa.Options, 'all' | 'encoding' | 'extendEnv' | 'env'>,
+) {
+  console.log(`Running ${cmdString}`);
+
+  const child = execa.command(cmdString, {
+    all: true,
+    encoding: 'utf8',
+    extendEnv: false,
+    env: {
+      PATH: process.env.PATH,
+      NPM_CONFIG_USERCONFIG: process.env.NPM_CONFIG_USERCONFIG,
+      NODE_AUTH_TOKEN: process.env.NODE_AUTH_TOKEN,
+      SCRATCH_PATH: process.env.SCRATCH_PATH,
+    },
+    ...options,
+  });
+
+  child.all?.pipe(process.stderr);
+
+  return child;
+}
+
 describe('bgdk create app', () => {
   let ctx: ResolveType<typeof setup>;
 
   const setup = async () => {
     await clearScratchSpace();
 
-    console.log(`Running npx bgdk@dev create ${SCRATCH_PATH}`);
-
-    const child = execa.command(`npx bgdk@dev create ${SCRATCH_PATH}`, {
-      all: true,
-      encoding: 'utf8',
-      extendEnv: false,
-      env: {
-        PATH: process.env.PATH,
-        NPM_CONFIG_USERCONFIG: process.env.NPM_CONFIG_USERCONFIG,
-        NODE_AUTH_TOKEN: process.env.NODE_AUTH_TOKEN,
-        SCRATCH_PATH: process.env.SCRATCH_PATH,
-      },
-    });
-
-    child.all?.pipe(process.stderr);
-
-    return { result: await child };
+    return { result: await cmd(`npm init bgdk@dev ${SCRATCH_PATH}`) };
   };
 
   beforeAll(async () => {
@@ -44,5 +52,33 @@ describe('bgdk create app', () => {
     const packagePath = path.resolve(SCRATCH_PATH, 'package.json');
     const packageStat = await fs.stat(packagePath);
     expect(packageStat.isFile()).toBe(true);
+  });
+
+  describe('yarn install', () => {
+    beforeAll(async () => {
+      await cmd(`yarn install`, {
+        cwd: SCRATCH_PATH,
+      });
+    });
+
+    it('installs successfully', async () => {
+      expect((await fs.readdir(SCRATCH_PATH)).sort()).toMatchInlineSnapshot(`
+        Array [
+          ".babelrc",
+          ".browserslistrc",
+          ".gitignore",
+          ".prettierrc",
+          ".storybook",
+          "CHANGELOG.md",
+          "README.md",
+          "jest.config.js",
+          "node_modules",
+          "package.json",
+          "src",
+          "tsconfig.json",
+          "yarn.lock",
+        ]
+      `);
+    });
   });
 });
