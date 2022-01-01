40143ca7ed3a52e6750c050cafbb477b14611c4d
Eamon Hyland <ehyland90@gmail.com>
2022-01-02 10:02:15 +1100

    resolve SCRATCH_PATH env

--- tests/e2e/test-utils.ts
+++ tests/e2e/test-utils.ts
@@ -5,7 +5,7 @@ export type ResolveType<T> = T extends (...args: any[]) => Promise<infer R>
   ? R
   : never;
 
-export const SCRATCH_PATH = process.env.SCRATCH_PATH!;
+export const SCRATCH_PATH = path.resolve(process.env.SCRATCH_PATH!);
 
 if (!SCRATCH_PATH) {
   throw new Error('Missing environment variable SCRATCH_PATH');
