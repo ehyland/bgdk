e304bc84bd0d24d4e24ad2f2846dce768c1789a9
Eamon Hyland <ehyland90@gmail.com>
2022-06-05 17:57:14 +1000

    Update dependencies

--- packages/bgdk/src/cli/index.ts
+++ packages/bgdk/src/cli/index.ts
@@ -22,35 +22,42 @@ prog
   .command('dev')
   .describe('Start dev server')
   .action(async () => {
-    await require('bgdk/commands/dev').main();
+    const module = await import('../commands/dev');
+    await module.main();
   });
 
 prog
   .command('build')
   .describe('Create a production build')
   .action(async () => {
-    await require('bgdk/commands/build').main();
+    const module = await import('../commands/build');
+    await module.main();
   });
 
 prog
   .command('serve')
   .describe('Serve app from ./dist')
   .action(async () => {
-    await require('bgdk/commands/serve').main();
+    const module = await import('../commands/serve');
+    await module.main();
   });
 
 prog
   .command('create <app-path>')
   .describe('Create a new project with bgdk')
   .action(async (appPath: string) => {
-    await require('bgdk/commands/create').main({ appPath });
+    const module = await import('../commands/create');
+    await module.main({ appPath });
   });
 
 prog
   .command('component <component-path>')
   .describe('Create a new component')
   .action(async (componentPath: string) => {
-    await require('bgdk/commands/component').main({ componentPath });
+    const module = await import('../commands/component');
+    await module.main({
+      componentPath,
+    });
   });
 
 prog.parse(process.argv);
